266b5e6ce64ac8f2dc7d0dac39f81b2a768a941b
DiAnne Lozano <diannelozano@DiAnnes-MacBook-Pro-2.local>
2021-09-13 15:18:44 -0700

    js revision with images

--- main.js
+++ main.js
@@ -1,153 +1,129 @@
 /*----- constants -----*/
-const dealerDeck = document.getElementById('#dealerStack');
-const playerDeck = document.getElementById('#playerStack');
+
+// Grab all the documents by all the Ids I'll need 
+
+// Then, change the source of images to hard code in 1 card. Taking out image card in the div. Change background image in css
+
 const deckRef = {
+    suits: ['clubs', 'diamonds', 'hearts', 'spades'],
+    faceVal: [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+}
+
+console.log(deckRef);
+
+const deckImages = {
     ace: {
-        imagesURL: ['images/clubs/clubs-A.svg', 'images/diamonds/diamonds-A.svg', 'images/hearts/hearts-A.svg', 'images/spades/spades-A.svg'],
-        lowerVal: ['clubsKing', 'clubsQueen', 'clubsJack', 'clubs10', 'clubs9', 'clubs8', 'clubs7', 'clubs6', 'clubs5', 'clubs4', 'clubs3', 'clubs2', 'diamondsKing', 'diamondsQueen', 'diamondsJack', 'diamonds10', 'diamonds9', 'diamonds8', 'diamonds7', 'diamonds6', 'diamonds5', 'diamonds4', 'diamonds3', 'diamonds2', 'heartsKing', 'heartsQueen', 'heartsJack', 'hearts10', 'hearts9', 'hearts8', 'hearts7', 'hearts6', 'hearts5', 'hearts4', 'hearts3', 'hearts2', 'spadesKing', 'spadesQueen', 'spadesJack', 'spades10', 'spades9', 'spades8', 'spades7', 'spades6', 'spades5', 'spades4', 'spades3', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-A.svg', 'card-deck-css/images/diamonds/diamonds-A.svg', 'card-deck-css/images/hearts/hearts-A.svg', 'card-deck-css/images/spades/spades-A.svg'],   
     },
     king: { 
-        imagesURL: ['images/clubs/clubs-K.svg', 'images/diamonds/diamonds-K.svg', 'images/hearts/hearts-K.svg', 'images/spades/spades-K.svg'],
-        lowerVal: ['clubsQueen', 'clubsJack', 'clubs10', 'clubs9', 'clubs8', 'clubs7', 'clubs6', 'clubs5', 'clubs4', 'clubs3', 'clubs2', 'diamondsQueen', 'diamondsJack', 'diamonds10', 'diamonds9', 'diamonds8', 'diamonds7', 'diamonds6', 'diamonds5', 'diamonds4', 'diamonds3', 'diamonds2', 'heartsQueen', 'heartsJack', 'hearts10', 'hearts9', 'hearts8', 'hearts7', 'hearts6', 'hearts5', 'hearts4', 'hearts3', 'hearts2', 'spadesQueen', 'spadesJack', 'spades10', 'spades9', 'spades8', 'spades7', 'spades6', 'spades5', 'spades4', 'spades3', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-K.svg', 'card-deck-css/images/diamonds/diamonds-K.svg', 'card-deck-css/images/hearts/hearts-K.svg', 'card-deck-css/images/spades/spades-K.svg'],
     },
     queen: {
-        imagesURL: ['images/clubs/clubs-Q.svg', 'images/diamonds/diamonds-Q.svg', 'images/hearts/hearts-Q.svg', 'images/spades/spades-Q.svg'],
-        lowerVal: ['clubsJack', 'clubs10', 'clubs9', 'clubs8', 'clubs7', 'clubs6', 'clubs5', 'clubs4', 'clubs3', 'clubs2', 'diamondsJack', 'diamonds10', 'diamonds9', 'diamonds8', 'diamonds7', 'diamonds6', 'diamonds5', 'diamonds4', 'diamonds3', 'diamonds2', 'heartsJack', 'hearts10', 'hearts9', 'hearts8', 'hearts7', 'hearts6', 'hearts5', 'hearts4', 'hearts3', 'hearts2', 'spadesJack', 'spades10', 'spades9', 'spades8', 'spades7', 'spades6', 'spades5', 'spades4', 'spades3', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-Q.svg', 'card-deck-css/images/diamonds/diamonds-Q.svg', 'card-deck-css/images/hearts/hearts-Q.svg', 'card-deck-css/images/spades/spades-Q.svg'],
     },
     jack: {
-        imagesURL: ['images/clubs/clubs-J.svg', 'images/diamonds/diamonds-J.svg', 'images/hearts/hearts-J.svg', 'images/spades/spades-J.svg'],
-        lowerVal: ['clubs10', 'clubs9', 'clubs8', 'clubs7', 'clubs6', 'clubs5', 'clubs4', 'clubs3', 'clubs2', 'diamonds10', 'diamonds9', 'diamonds8', 'diamonds7', 'diamonds6', 'diamonds5', 'diamonds4', 'diamonds3', 'diamonds2', 'hearts10', 'hearts9', 'hearts8', 'hearts7', 'hearts6', 'hearts5', 'hearts4', 'hearts3', 'hearts2', 'spades10', 'spades9', 'spades8', 'spades7', 'spades6', 'spades5', 'spades4', 'spades3', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-J.svg', 'card-deck-css/images/diamonds/diamonds-J.svg', 'card-deck-css/images/hearts/hearts-J.svg', 'card-deck-css/images/spades/spades-J.svg'],     
     },
     ten: {
-        imagesURL: ['images/clubs/clubs-r10.svg', 'images/diamonds/diamonds-r10.svg', 'images/hearts/hearts-r10.svg', 'images/spades/spades-r10.svg'],
-        lowerVal: ['clubs9', 'clubs8', 'clubs7', 'clubs6', 'clubs5', 'clubs4', 'clubs3', 'clubs2', 'diamonds9', 'diamonds8', 'diamonds7', 'diamonds6', 'diamonds5', 'diamonds4', 'diamonds3', 'diamonds2', 'hearts9', 'hearts8', 'hearts7', 'hearts6', 'hearts5', 'hearts4', 'hearts3', 'hearts2', 'spades9', 'spades8', 'spades7', 'spades6', 'spades5', 'spades4', 'spades3', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-r10.svg', 'card-deck-css/images/diamonds/diamonds-r10.svg', 'card-deck-css/images/hearts/hearts-r10.svg', 'card-deck-css/images/spades/spades-r10.svg'],
     },
     nine: {
-        imagesURL: ['images/clubs/clubs-r09.svg', 'images/diamonds/diamonds-r09.svg', 'images/hearts/hearts-r09.svg', 'images/spades/spades-r09.svg'],
-        lowerVal: ['clubs8', 'clubs7', 'clubs6', 'clubs5', 'clubs4', 'clubs3', 'clubs2', 'diamonds8', 'diamonds7', 'diamonds6', 'diamonds5', 'diamonds4', 'diamonds3', 'diamonds2', 'hearts8', 'hearts7', 'hearts6', 'hearts5', 'hearts4', 'hearts3', 'hearts2', 'spades8', 'spades7', 'spades6', 'spades5', 'spades4', 'spades3', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-r09.svg', 'card-deck-css/images/diamonds/diamonds-r09.svg', 'card-deck-css/images/hearts/hearts-r09.svg', 'card-deck-css/images/spades/spades-r09.svg'],
     },
     eight: {
-        imagesURL: ['images/clubs/clubs-r08.svg', 'images/diamonds/diamonds-r08.svg', 'images/hearts/hearts-r08.svg', 'images/spades/spades-r08.svg'],
-        lowerVal: ['clubs7', 'clubs6', 'clubs5', 'clubs4', 'clubs3', 'clubs2', 'diamonds7', 'diamonds6', 'diamonds5', 'diamonds4', 'diamonds3', 'diamonds2', 'hearts7', 'hearts6', 'hearts5', 'hearts4', 'hearts3', 'hearts2', 'spades7', 'spades6', 'spades5', 'spades4', 'spades3', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-r08.svg', 'card-deck-css/images/diamonds/diamonds-r08.svg', 'card-deck-css/images/hearts/hearts-r08.svg', 'card-deck-css/images/spades/spades-r08.svg'],
     },
     seven: {
-        imagesURL: ['images/clubs/clubs-r07.svg', 'images/diamonds/diamonds-r07.svg', 'images/hearts/hearts-r07.svg', 'images/spades/spades-r07.svg'],
-        lowerVal: ['clubs6', 'clubs5', 'clubs4', 'clubs3', 'clubs2', 'diamonds6', 'diamonds5', 'diamonds4', 'diamonds3', 'diamonds2', 'hearts6', 'hearts5', 'hearts4', 'hearts3', 'hearts2', 'spades6', 'spades5', 'spades4', 'spades3', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-r07.svg', 'card-deck-css/images/diamonds/diamonds-r07.svg', 'card-deck-css/images/hearts/hearts-r07.svg', 'card-deck-css/images/spades/spades-r07.svg'],
     },
     six: {
-        imagesURL: ['images/clubs/clubs-r06.svg', 'images/diamonds/diamonds-r06.svg', 'images/hearts/hearts-r06.svg', 'images/spades/spades-r06.svg'],
-        lowerVal: ['clubs5', 'clubs4', 'clubs3', 'clubs2', 'diamonds5', 'diamonds4', 'diamonds3', 'diamonds2', 'hearts5', 'hearts4', 'hearts3', 'hearts2', 'spades5', 'spades4', 'spades3', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-r06.svg', 'card-deck-css/images/diamonds/diamonds-r06.svg', 'card-deck-css/images/hearts/hearts-r06.svg', 'card-deck-css/images/spades/spades-r06.svg'],
     },
     five: {
-        imagesURL: ['images/clubs/clubs-r05.svg', 'images/diamonds/diamonds-r05.svg', 'images/hearts/hearts-r05.svg', 'images/spades/spades-r05.svg'],
-        lowerVal: ['clubs4', 'clubs3', 'clubs2', 'diamonds4', 'diamonds3', 'diamonds2', 'hearts4', 'hearts3', 'hearts2', 'spades4', 'spades3', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-r05.svg', 'card-deck-css/images/diamonds/diamonds-r05.svg', 'card-deck-css/images/hearts/hearts-r05.svg', 'card-deck-css/images/spades/spades-r05.svg']
     },
     four: {
-        imagesURL: ['images/clubs/clubs-r04.svg', 'images/diamonds/diamonds-r04.svg', 'images/hearts/hearts-r04.svg', 'images/spades/spades-r04.svg'],
-        lowerVal: ['clubs3', 'clubs2', 'diamonds3', 'diamonds2', 'hearts3', 'hearts2', 'spades3', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-r04.svg', 'card-deck-css/images/diamonds/diamonds-r04.svg', 'card-deck-css/images/hearts/hearts-r04.svg', 'card-deck-css/images/spades/spades-r04.svg'],
     },
     three: {
-        imagesURL: ['images/clubs/clubs-r03.svg', 'images/diamonds/diamonds-r03.svg', 'images/hearts/hearts-r03.svg', 'images/spades/spades-r03.svg'],
-        lowerVal: ['clubs2', 'diamonds2', 'hearts2', 'spades2']
+        imagesURL: ['card-deck-css/images/clubs/clubs-r03.svg', 'card-deck-css/images/diamonds/diamonds-r03.svg', 'card-deck-css/images/hearts/hearts-r03.svg', 'card-deck-css/images/spades/spades-r03.svg'],
     },
     two: {
-        imagesURL: ['images/clubs/clubs-r02.svg', 'images/diamonds/diamonds-r02.svg', 'images/hearts/hearts-r02.svg', 'images/spades/spades-r02.svg']
+        imagesURL: ['card-deck-css/images/clubs/clubs-r02.svg', 'card-deck-css/images/diamonds/diamonds-r02.svg', 'card-deck-css/images/hearts/hearts-r02.svg', 'card-deck-css/images/spades/spades-r02.svg']
     },
 }
-console.log(deckRef);
+console.log(deckImages);
+
 
 /*----- app's state (variables) -----*/
 
-let splitDeck;
-let cardsWon;
-let faceCards;
-let winner;
-let war;
+let deal; // button will hide when clicked
+let splitDeck; //  deck split into 26 cards for dealer and player
+let cardsWon; // will change every time player or dealer wins war
+let faceCards; // will change when you click on top of stacked card deck
+let winner; // = null until end of game
+let war; // null until war is declared
+let dealAgain; // to replay game
+
 
 /*----- cached element references -----*/
-const splitDeckEls = {
-    dealer: document.querySelector('#dealerStack > img'),
-    player: document.querySelector('#playerStack > img')
-
+const dealer = document.querySelector('DEALER');
+const player = document.querySelector('PLAYER');
+const dealerDeckEls = {
+    dealer: document.querySelector('#dealerStack'),
+    player: document.querySelector('#playerStack')
+}
+const faceCardEls = {
+    dealer: document.querySelector('#dealerFaceCard'),
+    player: document.querySelector('#playerFaceCard')
 }
 const cardsWonEls = {
     dealer: document.querySelector('#dealerWon'),
     player: document.querySelector('#playerWon')
 }
-
-const faceCardEls = {
-    dealer: {
-        imagesEl: document.querySelector('#dealerFaceCard > img')
-    },
-    player: {
-        imagesEl: document.querySelector('#playerFaceCard > img')
-    }
-} 
+const splitDeckEls = {
+    dealer: document.querySelector('#dealerStack'),
+    player: document.querySelector('#playerStack')
+}
 
 /*----- event listeners -----*/
 document.querySelector('button')
     .addEventListener('click', dealCards);
 
-document.querySelector('#playerStack')   
-    .addEventListener('click', displayCard);
-
-    
 /*----- functions -----*/
-init()
 
-function init(){
-    cardsWon = {
-        dealer: 0,
-        player: 0,
+// function init(){
+//     // splitDeck = {
+//     //     dealer: 
+//     // }
+
+//     render()
+// }
+// function render(){
+
+    function dealCards(){
+        console.log('Draw Cards');
     }
-   
-    faceCards = {
-        dealer: 'clubsKing',
-        player: 'clubsKing'
-    }
+// }
+// Player clicks on Deal button to start game  and deal cards
+// Then, deal button needs to disappear from screen
 
-    winner = null;
-    war = null;
+//  When player clicks on top of their card <div><img>Dealer’s card played displays, get random card
 
-    render()
-}
-
-function render(){
-
-     
-    for(let cardWon in cardsWon){
-        console.log(cardWon, '< key name');
-    cardsWonEls[cardWon].textContent = cardsWon[cardWon];
-    }
-
-    for(let faceCard in faceCards){
-        console.log(faceCard, 'clubsKing');
-        faceCardEls[faceCard].imagesEl.src = deckRef[faceCards[faceCard]].imagesUrl
-    }
-}
-
-function dealCards(){
-    console.log('Draw Card'); 
-} 
-
-function displayCard(){
-    console.log('Card Drawn');
-}
-
-// if (dealer > player) {
-
-// }else if (player > dealer)
+// Cards won score changes as winner collects winning cards
 
 
-
-
-
-
-// faceCards.dealer = getRandomCard();
-// faceCards.player = getRandomCard();
-    
-// const randomIdx = Math.floor(Math.random() * 26);
-//     return choice[randomIdx]
-
-  
-
+// If player draws a higher value, then player gets both cards - Highest to lowest values (A, K, Q, J, 10, 9, 8, 7, 6, 5, 4, 3, 2)
+// If player draws a lower value, then dealer gets both cards
+// If player draws a card of the same value as dealer, then a war is declared. 
+// Function: WAR will display (pop up) and disappear after 3 seconds
+// Player clicks on top of their top card 4 times
+// 4th card displays face up. 
+// If player draws a higher value, then player gets both cards - Highest to lowest values (A, K, Q, J, 10, 9, 8, 7, 6, 5, 4, 3, 2)
+// If player draws a lower value, then dealer gets both cards
+// If player draws a card of the same value as dealer, then a war is declared.
+// If war happens again, repeat steps above
+// Winner of the war, gets all cards
+// Game will need to loop until winner gets all 52 cards
+// When games ends, display winner
+// <button>DEAL AGAIN
