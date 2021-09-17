7c35cb4043d9130c56b460a61b4427f812807034
DiAnne Lozano <diannelozano@DiAnnes-MacBook-Pro-2.local>
2021-09-16 18:25:37 -0700

    redefine playerHand

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# project-1-devwar
+DEV WAR
+
+Take a chance at battle against the Dealer in this simple renaissance themed card game called DEV WAR. It’s all about chance and being the first to win all 52 cards. 
+
+https://ditabu.github.io/project-1-devwar
+
+To Play
+Click on the DEAL button to start the game
+Click on the top of your stack of cards (the blue color) to play
+Highest to lowest values are as follows: A, K, Q, J, 10, 9, 8, 7, 6, 5, 4, 3, 2
+If you draw a higher value, then you get both cards
+If you draw a lower value, then dealer gets both cards
+If you draw a card of the same value as dealer, then a war is declared. 
+Click the top of your card 4 times, your 4th card will determine your fate at war
+If you draw a higher value, then you get all cards
+If you draw a lower value, then dealer gets all cards
+If you draw a card of the same value as dealer, then a war is declared again...
+Keep playing until you or the dealer wins all 52 cards! 
+
+I plan to improve the look and feel of the game, as well as the functionality. Right now, the game is very simple. In the future, there will be animations to the game such as having the card flip over to reveal its value, options for choosing card colors, battle names and some css styling to it.
+
--- main.js
+++ main.js
@@ -13,17 +13,16 @@ let playerCard;
 let dealerCard;
 
 /*----- cached element references -----*/
+const btn = document.querySelector('button');
+console.log(btn);
+
 const dealerDeckEls = {
     dealer: document.querySelector('#dealerStack'),
     player: document.querySelector('#playerStack')
 }
 const faceCardEls = {
-    dealer: {
-        imagesEl: document.querySelector('#dealerFaceCard > img')
-    },    
-    player:  {
-        imagesEl: document.querySelector('#playerFaceCard > img')
-    }    
+    dealer: document.querySelector('#dealerFaceCard'),   
+    player: document.querySelector('#playerFaceCard') 
 }
 const cardsWonEls = {
     dealer: document.querySelector('#dealerWon'),
@@ -35,8 +34,8 @@ const splitDeckEls = {
 }
 
 /*----- event listeners -----*/
-document.querySelector('button')
-.addEventListener('click', dealCards); 
+btn.addEventListener('click', dealCards); 
+// btn.removeEventListener('click', dealCards);
 
 document.querySelector('#playerStack')
 .addEventListener('click', compareCards);
@@ -44,17 +43,40 @@ document.querySelector('#playerStack')
 document.querySelector('#playerStack')
 .addEventListener('click', displayRandomCard);
 
-document.querySelector('#playerStack')
-.addEventListener('click', gameOver);
+// document.querySelector('#playerStack')
+// .addEventListener('click', gameOver);
 
 /*----- functions -----*/
+function init(){
+    cardsWon = {
+        dealer: 0,
+        player: 0,
+    }
+    
+    faceCards = {
+        dealer: 0,
+        player: 0,
+    }
+    
+    winner = null;
+    war = null;
+
+    playerHand = [];
+    dealerHand = [];
+
+    discardPlayer = [];
+    discardDealer = [];
+
+    render();
+}
+
 function createNewDeck(){
     faceVal = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
     suits = ['♣', '♦', '♥', '♠'];
-        for(let a = 0; a < suits.length; a++) {
+        for(let i = 0; i < suits.length; i++) {
             for(let f = 0; f < faceVal.length; f++) {
                 const value = faceVal[f];
-                const suit = suits[a];
+                const suit = suits[i];
                 deck.push({ value, suit });
             }
         }
@@ -89,11 +111,10 @@ function dealCards(){
     splitDeck();
     displayRandomCard();
 }    
-// }
-// function disableDealBtn(){
-//     document.querySelector('button').disabled = true;
-//     disableDealBtn();
-// }
+ 
+function disableDealBtn(){
+    disableDealBtn();
+}
 
 
 function displayRandomCard(){
@@ -101,10 +122,15 @@ function displayRandomCard(){
     dealerCard = dealerHand[0]
     console.log(playerCard, dealerCard, "these are my cards")
     const dealerFaceCard = document.getElementById('dealerFaceCard')
-    dealerFaceCard.innerHTML = `${dealerCard.value}${dealerCard.suit}`
+    dealerFaceCard.innerHTML = `${dealerCard?.value}${dealerCard?.suit}`
     const playerFaceCard = document.getElementById('playerFaceCard')
-    playerFaceCard.innerHTML = `${playerCard.value}${playerCard.suit}`
+    playerFaceCard.innerHTML = `${playerCard?.value}${playerCard?.suit}` //copy this for check winner 
+    // playerCard.value = 'No more cards'
+    // dealerCard.value = 'No more cards'
+    // playerCard.suit = 'No more cards'
+    // dealerCard.suit = 'No more cards'
 }
+
 
 function compareCards(){
     console.log(playerCard, dealerCard, 'compare card function')
@@ -122,6 +148,8 @@ function compareCards(){
         console.log('pc lower dc', playerHand.length, dealerHand.length,discardDealer.length, discardPlayer.length)
     }else if(playerCard.value === dealerCard.value) {
         war = 'WAR';
+        // select element on html where I'm going to leave a message for user letting them know that war is happeneing and update text content
+        // then, 
         discardDealer.push(dealerCard);
         discardPlayer.push(playerCard);
         dealerHand.shift();
@@ -131,6 +159,17 @@ function compareCards(){
             console.log('war declared');
         }
     }
+    if (playerHand.length === 0){
+        playerHand = discardPlayer
+        discardPlayer = []
+        console.log(playerHand, discardPlayer, 'Empty Player Cards')
+    }
+    if (dealerHand.length === 0){
+        dealerHand = discardDealer
+        discardDealer = []
+        console.log(dealerHand, discardPlayer, 'Empty Player Cards')
+    }
+    checkWinner();
 }
 console.log(dealerHand, playerHand)
 
@@ -150,6 +189,9 @@ function declareWarWin(){
         console.log('pc lower dc', playerHand.length, dealerHand.length,discardDealer.length, discardPlayer.length)
     }else if(playerCard.value === dealerCard.value) {
         war = 'WAR';
+        playerFaceCard.textContent = 'WAR is DECLARED!';
+        // select element on html where I'm going to leave a message for user letting them know that war is happeneing and update text content
+        // then, 
         discardDealer.push(dealerCard);
         discardPlayer.push(playerCard);
         dealerHand.shift();
@@ -162,40 +204,28 @@ function declareWarWin(){
 
 }
 
-function gameOver(){
-    if(playerHand === 0) {
-        winner = 'dealer'
-    }else if(dealerHand === 0) {
-        looser = 'player'
+function checkWinner(){
+    if(discardPlayer.length === 0 && playerHand.length === 0) {
+        console.log(discardPlayer.length, discardDealer.length, 'winner?')
+        dealer = `You lost the battle!`
+        document.querySelector('#dealerWon').innerHTML = 'Dealer Wins War!';
+    }else if(discardDealer.length === 0 && dealerHand.length === 0) {
+        player = `You won the battle!`
+        // playerCard.textContent = 'Player Wins War!';
     }
 }
-
-function init(){
-    cardsWon = {
-        dealer: 0,
-        player: 0,
-    }
-    
-    faceCards = {
-        dealer: 0,
-        player: 0,
-    }
-    
-    winner = null;
-    looser = null;
-    war = null;
-
-    playerHand = [];
-    dealerHand = [];
-
-    render()
+function checkEmptyHand(){
+    // run this function if player hand = 0, then redefine playerHand = playerDiscard
+    // then, change the discardplayer = []
+    // if both playerhand && discardPlayer are [], playerHand === [] && discardPlayer === [], then they lost
+    // same thing for the dealer
+    // or keep track of a variable allPlayerCards = playerHand.length + playerdiscard.length, player wins 
 }
-
-function render(){   
-    // Cards won score changes as winner collects winning cards
-    for(let cardWon in cardsWon){
-        console.log(cardWon, '< key name');
-    cardsWonEls[cardWon].textContent = cardsWon[cardWon];
-    }
-    render()
-}
+// // function render(){   
+//     // Cards won score changes as winner collects winning cards
+//     for(let cardWon in cardsWon){
+//         console.log(cardWon, '< key name');
+//     cardsWonEls[cardWon].textContent = cardsWon[cardWon];
+//     }
+//     // render();
+// // }
